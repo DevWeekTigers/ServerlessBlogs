2b386393d301f900c82077e53b2271eeecd434ba
Danielxburns <danielburnsart@gmail.com>
2022-08-19 14:28:46 -0500

    FE initial commit

--- frontend/serverlessblogs/src/App.js
+++ frontend/serverlessblogs/src/App.js
@@ -16,6 +16,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          and then create a serverless bloggin app
         </a>
       </header>
     </div>
