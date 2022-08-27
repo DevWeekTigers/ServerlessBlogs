4da3d2dd3381b7a10deae642297c87c466c89fad
Danielxburns <danielburnsart@gmail.com>
2022-08-27 16:31:08 -0500

    make component borders multi-colored

--- frontend/src/routes/navBar.jsx
+++ frontend/src/routes/navBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
       <div className="nav-container">
         <Link to="/">Home</Link>{' '}
         <Link to="/auth">Sign-In/Sign-Up</Link>{' '}
-        <Link to="/user">My Posts</Link>{' '}
+        <Link to="/user">User</Link>{' '}
         <Link to="/admin">Admin</Link>{' '}
       </div>
       <Outlet />
