aeb1614db8dd7e8bbcd2b8498cfe6a764d4db1ae
Si Lam <silam@hotmail.com>
2022-08-29 12:24:13 -0500

    Merge main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './routes/navBar';
+//import NavBar from './routes/navBar';
 import HomePage from './routes/homePage';
 import AuthPage from './routes/authPage';
 import UserPage from './routes/userPage';
 import AdminPage from './routes/adminPage';
 //import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './routes/navBar';
+
 
 function App() {
+  
   return (
-    <Routes>
-      <Route path="/" element={<NavBar />}>
-        <Route index element={<HomePage />} />
-        <Route path="/user"element={<UserPage />} />
-        <Route path="/auth"element={<AuthPage />} />
-        <Route path="/admin"element={<AdminPage />} />
-      </Route>
-    </Routes>
+    
+     <>
+     <Navbar />
+      <Routes>
+         <Route index element={<HomePage />} />
+         <Route path="/user" element={<UserPage />} />
+         <Route path="/auth" element={<AuthPage />} />
+         <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+     </>
+      
+    
   );
+
 }
 
 export default App;
--- frontend/src/routes/navBar.jsx
+++ frontend/src/routes/navBar.jsx
@@ -1,18 +1,54 @@
 import { Outlet, Link } from 'react-router-dom';
 import '../App.css';
+import Container from 'react-bootstrap/Container';
+//import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Switch } from "react-router";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './navBarElements';
+
 
 const NavBar = () => {
+  
   return (
-    <div className="App">
-      <div className="nav-container">
-        <Link to="/">Home</Link>{' '}
-        <Link to="/auth">Sign-In/Sign-Up</Link>{' '}
-        <Link to="/user">My Posts</Link>{' '}
-        <Link to="/admin">Admin</Link>{' '}
-      </div>
-      <Outlet />
-    </div>
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          
+          <NavLink to='/home' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/blogs' activeStyle>
+            User Blogs
+          </NavLink>
+          <NavLink to='/admin' activeStyle>
+            Admin
+          </NavLink>
+         
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign-In/Sign-Up</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
   );
+  
+
+  
 };
 
 export default NavBar;
