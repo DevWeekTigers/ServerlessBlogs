5c9cf825902e6be66c4e828fa30c2857d0acdad1
Danielxburns <danielburnsart@gmail.com>
2022-09-05 13:54:26 -0500

    get categories from DB and set inPostsContext

--- frontend/src/contexts/postsContext.jsx
+++ frontend/src/contexts/postsContext.jsx
@@ -4,22 +4,31 @@ import axios from 'axios';
 export const PostsContext = createContext({
   posts: [],
   setPosts: () => {},
+  categories: [],
+  setCategories: () => {},
 });
 
 export const PostsProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
-
+  const [categories, setCategories] = useState([])
   
   const getPostsData = async () => {
     const posts  =  (await axios.get('http://localhost:5000/api/posts')).data;
     setPosts(posts);
   };
 
+  const getCategories = async () => {
+    const categories = (await axios.get('http://localhost:5000/api/posts/categories')).data;
+    console.log('inside getCategories - categories :>> ', categories);
+    setCategories(categories);
+  }
+
   useEffect(() => {
     getPostsData();
+    getCategories();
   }, []);
 
-  const value = { posts, setPosts };
+  const value = { posts, setPosts, categories };
   return (
     <PostsContext.Provider value={value}>{children}</PostsContext.Provider>
   );
