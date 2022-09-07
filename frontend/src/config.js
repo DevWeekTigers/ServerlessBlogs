99a9b975b0337e61780418d6fd8b176b8d27c8f8
Si Lam <silam@hotmail.com>
2022-09-07 00:58:26 -0500

    Fix env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
 
-const BACK_END_URL = process.env.REACT_APP_BACKEND_API_URL || "https://serverlessblogsapi.azurewebsites.net"
-const BACK_END_PORT = process.env.REACT_APP_BACKEND_PORT || "80"
-
-export const API_ENDPOINT = BACK_END_URL+ ":" + BACK_END_PORT
+export const API_ENDPOINT = process.env.REACT_APP_BACKEND_API_URL || "https://serverlessblogsapi.azurewebsites.net"
