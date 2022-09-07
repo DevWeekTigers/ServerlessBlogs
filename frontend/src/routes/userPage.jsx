16514c84446a683ccd88d39804897214b453523c
Danielxburns <danielburnsart@gmail.com>
2022-09-07 11:04:47 -0500

    initial re-styling commit

--- frontend/src/routes/userPage.jsx
+++ frontend/src/routes/userPage.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Profile from '../components/profile'
+import Profile from '../components/profile';
 import PrevPosts from '../components/prev-posts';
 import NewPost from '../components/new-post';
 
 const UserPage = () => {
-  return(
+  return (
     <div className="user-page page">
       <p>User page</p>
-      <Profile />
-      <NewPost />
+      <div className="posts-container">
+        <Profile />
+        <NewPost />
+      </div>
       <PrevPosts />
     </div>
-  )
+  );
 };
 
 export default UserPage;
