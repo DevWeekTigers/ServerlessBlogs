250403328c863ae43e84ba938ebab0366108fee7
Si Lam <silam@hotmail.com>
2022-09-07 00:38:02 -0500

    Fix env for prod

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,5 @@
-export const API_ENDPOINT = process.env.REACT_APP_BACKEND_API_URL + ":" + process.env.REACT_APP_BACKEND_PORT
+
+const BACK_END_URL = process.env.REACT_APP_BACKEND_API_URL || "https://serverlessblogsapi.azurewebsites.net"
+const BACK_END_PORT = process.env.REACT_APP_BACKEND_PORT || "80"
+
+export const API_ENDPOINT = BACK_END_URL+ ":" + BACK_END_PORT
